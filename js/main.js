1fc999c397a83129a071cfcc705f699aedd40d8f
JP10801 <64243159+JP10801@users.noreply.github.com>
2026-05-27 22:47:16 -0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const stars = document.getElementById('stars');
+
+for(let i = 0; i < 120; i++){
+
+  const star = document.createElement('div');
+
+  star.classList.add('star');
+
+  star.style.left = Math.random() * 100 + '%';
+  star.style.top = Math.random() * 100 + '%';
+
+  star.style.animationDelay =
+    Math.random() * 5 + 's';
+
+  star.style.opacity = Math.random();
+
+  stars.appendChild(star);
+}
